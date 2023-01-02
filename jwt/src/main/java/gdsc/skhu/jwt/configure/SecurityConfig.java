40f0df67768483f568fc7268dce6ad96d2f212af
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-01-02 16:58:00 +0900

    Comment : jwt SecurityConfig.java 주석 달기

--- jwt/src/main/java/gdsc/skhu/jwt/configure/SecurityConfig.java
+++ jwt/src/main/java/gdsc/skhu/jwt/configure/SecurityConfig.java
@@ -23,17 +23,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+                http
                 .httpBasic().disable()
+                // csrf : 인터넷 사용자(희생자)가 자신의 의지와는 무관하게
+                // 공격자가 의도한 행위(수정, 삭제, 등록 등)를 특정 웹사이트에 요청하게 만드는 공격
+                // 왠만하면 끄지말자 대신 csrf 토큰이랑 같이 보내야함
                 .csrf().disable()
+                // .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.정책상수)
+                //sessionCreationPolicy : 스프링시큐리티 세션정책
+                // SessionCreationPolicy.STATELESS : 스프링시큐리티가 생성하지도않고 기존것을 사용하지도 않음
+                // ->JWT 같은토큰방식을 쓸때 사용하는 설정
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers("/index", "/login").permitAll()
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .authorizeRequests()// Request에 인증, 인가(권한 있냐?)를 부여하겠다.
+                .antMatchers("/index", "/login").permitAll()  // /index /login 은 인가가 필요없다.
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")// /user  uri는 USER 롤 또는 ADMIN 롤이 있어야 접속가능
+                .antMatchers("/admin").hasRole("ADMIN") // /admin uri는 ADMIN 롤이 있어야 접속가능.
+                .anyRequest().authenticated() // 그 외에는 인증된 모든 사용자가 URL을 허용하도록 지정합니다.
                 .and()
+                // JwtFilter 를 등록한다.
+                // SecurityContext를 사용하기 때문에 앞단의 필터에서 SecurityContext가 설정되고 난뒤 필터를 둔다.
                 .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
